cacf94d70e8fa7247c440a76d368651f6c7ea247
viktori <viktori@kth.se>
2021-10-12 20:34:40 +0200

    Added coordinates and alternative bars

--- frontend/assets/data/demo.ts
+++ frontend/assets/data/demo.ts
@@ -12,7 +12,7 @@ import IMAGE_10 from "../images/10.jpg";
 
 const data: DataT[] = [
   {
-    id: 100,
+    id: '1Dq5inAHn4elueza5Ob2WQ',
     name: "Low and Slow Smokehouse",
     
     // reviews
@@ -28,11 +28,62 @@ const data: DataT[] = [
     address: 'Luntmakargatan 98',
     distance: "Distance to restaurant: 1699m",
     isOpen: true,
+    latitude: 59.34413,
+    longitude: 18.05715, 
     
     // Optionals
     price: '$$',
     info1: 'Beer Bar',
     info2: 'BBQ',
+  },
+  {
+    id: '1z4GoFCju_U-t2Htm9CKqQ',
+    name: 'Pharmarium',
+    
+    // reviews
+    rating: '4.0',
+    review_count: '101',
+
+    // links
+    url: 'https://www.yelp.com/biz/pharmarium-stockholm?adjust_creative=TOa8a0g6nrrOYYfuEq-hAA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=TOa8a0g6nrrOYYfuEq-hAA',
+    image_url: 'https://s3-media2.fl.yelpcdn.com/bphoto/Bx3QYClk_yKcw6G8zHGWVQ/o.jpg',
+
+    // contacts/location
+    phone: '+46 8 20 08 10',
+    address: 'Stortorget 7',
+    distance: "Distance to restaurant: 675m",
+    isOpen: true,
+    latitude: 59.3248995919235,
+    longitude: 18.071048296988, 
+    
+    // Optionals
+    price: '$$',
+    info1: 'Cocktail Bars',
+    info2: 'Modern European',
+  },
+  {
+    id: 'f7Syjwdthv_hD-TydWPBNw',
+    name: 'Dovas',
+    
+    // reviews
+    rating: '3.0',
+    review_count: '11',
+
+    // links
+    url: 'https://www.yelp.com/biz/dovas-stockholm-2?adjust_creative=TOa8a0g6nrrOYYfuEq-hAA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=TOa8a0g6nrrOYYfuEq-hAA',
+    image_url: 'https://s3-media3.fl.yelpcdn.com/bphoto/BmGV6mEleEUzT6AI7_nBxg/o.jpg',
+
+    // contacts/location
+    phone: '+46 8 650 80 49',
+    address: 'Sankt Eriksgatan 53 A',
+    distance: "Distance to restaurant: 1823m",
+    isOpen: true,
+    latitude: 59.33561,
+    longitude: 18.03321, 
+    
+    // Optionals
+    price: '$',
+    info1: 'Pubs',
   },
   
 ];
